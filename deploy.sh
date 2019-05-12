964352335f3f48cdebd0812c1b0c66d220f6fef8
Pedro <pedrokvb@gmail.com>
2019-05-12 12:40:42 -0300

    fix deploy stage

--- deploy.sh
+++ deploy.sh
@@ -1,9 +1,6 @@
 #/bin/bash
-
 cd terraform
 terraform init
 terraform plan -out=plan
 terraform apply plan
 rm plan
-
-# aws apigateway create-deployment --rest-api-id `(cd terraform; terraform output api_id)` --stage-name `(cd terraform; terraform output api_stage_name)`
--- terraform/api.tf
+++ terraform/api.tf
@@ -4,8 +4,9 @@ resource "aws_api_gateway_rest_api" "api" {
 }
 
 resource "aws_api_gateway_deployment" "deployment" {
-  rest_api_id = "${aws_api_gateway_rest_api.api.id}"
-  stage_name  = "prod"
+  rest_api_id       = "${aws_api_gateway_rest_api.api.id}"
+  stage_name        = "prod"
+  stage_description = "${md5(file("api.tf"))}"             #Forçar atualização do stage
 }
 
 module "raskon_cep" {

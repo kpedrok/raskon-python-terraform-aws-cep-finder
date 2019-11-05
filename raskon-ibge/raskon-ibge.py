34ccc6433f4fc14b5b128c6da75d4521e499dfbc
Pedro B <pedrokvb@gmail.com>
2019-11-05 17:20:29 -0300

    ibge fix

--- raskon-ibge/raskon-ibge.py
+++ raskon-ibge/raskon-ibge.py
@@ -17,13 +17,14 @@ class DecimalEncoder(json.JSONEncoder):
                 return int(o)
         return super(DecimalEncoder, self).default(o)
 
-def find_ibge(uf,cidade):
+
+def find_ibge(uf, cidade):
     with open('ibge.json', encoding='utf-8-sig') as codigos_ibge:
         codigos_ibge = json.load(codigos_ibge)
         for cod in codigos_ibge:
             chave = str(formatar_texto(str(uf+cidade)))
             chave_json = formatar_texto(cod['Chave'])
-            if  chave_json == chave:
+            if chave_json == chave:
                 return (cod['IBGE'])
 
 
@@ -36,7 +37,6 @@ def buscar_ibge(uf, cidade):
     uf = formatar_texto(uf)
     cidade = formatar_texto(cidade)
     url = "https://cidades.ibge.gov.br/brasil/" + uf + "/" + cidade + "/panorama"
-    print(url)
 
     response = requests.request("GET", url)
     ibge = response.text
@@ -46,12 +46,27 @@ def buscar_ibge(uf, cidade):
     return (ibge)
 
 
-def lambda_handler(event, context):
-    try:
-        buscar_ibge(event['queryStringParameters']['uf'],
-                    event['queryStringParameters']['cidade'])
-        print(1)
+def servicos_ibge(uf, cidade):
+    url = "https://servicodados.ibge.gov.br/api/v1/localidades/municipios/"
 
+    response = requests.request("GET", url)
+
+    lista_ibge = response.text
+    lista_ibge = json.loads(lista_ibge)
+
+    for linha in lista_ibge:
+        if formatar_texto(uf) == formatar_texto(linha['microrregiao']['mesorregiao']['UF']['sigla']) and formatar_texto(cidade) == formatar_texto(linha['nome']):
+            # print(uf,cidade)
+            # print(linha['id'], linha['nome'],linha['microrregiao']['mesorregiao']['UF']['sigla'])
+            return (linha['id'])
+
+
+def lambda_handler(event, context):
+    print('buscar_ibge')
+    try:
+        ibge = buscar_ibge(event['queryStringParameters']['uf'],
+                           event['queryStringParameters']['cidade'])
+        print(1, ibge)
         return {
             "statusCode": 200,
             'headers': {
@@ -59,33 +74,51 @@ def lambda_handler(event, context):
             },
             "body": json.dumps(ibge, sort_keys=True,  ensure_ascii=False, indent=2, cls=DecimalEncoder),
         }
+
     except:
+        print('find_ibge')
         try:
             ibge = find_ibge(event['queryStringParameters']['uf'],
-                        event['queryStringParameters']['cidade'])
-            print(ibge)
+                             event['queryStringParameters']['cidade'])
+            print(2, ibge)
+            if ibge == None:
+                raise ValueError('find_ibge didnt work')
+
             return {
                 "statusCode": 200,
                 'headers': {
                     'Access-Control-Allow-Origin': '*'
                 },
-                "body": json.dumps(ibge, sort_keys=True,  ensure_ascii=False, indent=2, cls=DecimalEncoder),}
+                "body": json.dumps(ibge, sort_keys=True,  ensure_ascii=False, indent=2, cls=DecimalEncoder), }
+        except:
+            print('servicos_ibge')
+            try:
+                ibge = servicos_ibge(event['queryStringParameters']['uf'],
+                                     event['queryStringParameters']['cidade'])
+                print(3, ibge)
+               
+                return {
+                    "statusCode": 200,
+                    'headers': {
+                        'Access-Control-Allow-Origin': '*'
+                    },
+                    "body": json.dumps(ibge, sort_keys=True,  ensure_ascii=False, indent=2, cls=DecimalEncoder),
+                }
 
-
-        except Exception as e:
-            print(e)
-            return {
-                "statusCode": 400,
-                'headers': {
-                    'Access-Control-Allow-Origin': '*'
-                },
-                "body": json.dumps(e, sort_keys=True,  ensure_ascii=False, indent=2, cls=DecimalEncoder),
-            }
+            except Exception as e:
+                print(e)
+                return {
+                    "statusCode": 400,
+                    'headers': {
+                        'Access-Control-Allow-Origin': '*'
+                    },
+                    "body": json.dumps(e, sort_keys=True,  ensure_ascii=False, indent=2, cls=DecimalEncoder),
+                }
 
 
 # lambda_handler({
 #     "queryStringParameters": {
 #         "uf": "SP",
-#         "cidade": "São Paulo"
+#         "cidade": "Carapicuíba"
 #     }
 # }, "")
---
+++ teste-ibge.py
@@ -0,0 +1,26 @@
+import requests
+import json
+url = "https://servicodados.ibge.gov.br/api/v1/localidades/municipios/"
+
+headers = {
+    'User-Agent': "PostmanRuntime/7.19.0",
+    'Accept': "*/*",
+    'Cache-Control': "no-cache",
+    'Postman-Token': "46cab9e1-fa41-43fa-95a6-4cb78287ef8a,2f2afc54-c954-4f02-a042-0bf5206fe27f",
+    'Host': "servicodados.ibge.gov.br",
+    'Accept-Encoding': "gzip, deflate",
+    'Cookie': "ROUTEID=.sdadosSSL2",
+    'Connection': "keep-alive",
+    'cache-control': "no-cache"
+    }
+
+response = requests.request("GET", url, headers=headers)
+
+
+
+lista_ibge = response.text
+lista_ibge = json.loads(lista_ibge)
+for linha in lista_ibge:
+    print(linha['id'])
+    print(linha['nome'])
+    print(linha['microrregiao']['mesorregiao']['UF']['sigla'])
